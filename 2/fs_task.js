ca7ec37efbffca2519a4c2d2a335128f2af9256e
RukInDaHouse <r1997p@mail.ru>
2020-10-27 03:10:29 +0300

    FINALLLLLL

--- 2/fs_task.js
+++ 2/fs_task.js
@@ -1,22 +1,23 @@
 const input = document.querySelector('input')
 const paragraphs = document.querySelectorAll('a')
+const block = document.querySelector(".dropdown");
 
 input.addEventListener('input', e => {
   const searchTerm = e.target.value
   const regex = new RegExp(searchTerm, 'g')
   const replacement = `<span class="highlighted">${ searchTerm }</span>`
-
+  block.innerHTML = "";
   for (const a of paragraphs) {
-    if (a.innerText.match(regex) == null) {
-      a.style.display = '';
-    } else {
-      a.innerHTML = a.innerText.replace(regex, replacement)
+    if (a.innerText.match(regex) && searchTerm) {
+      a.innerText = a.innerText + "<br/>";
+      a.innerHTML = a.innerText.replace(regex, replacement);    
+      block.insertAdjacentElement("beforeend", a.cloneNode(true));
     }
   }
-
   let text = document.body.textContent
   let separator = searchTerm
   let number = WordCount(text, separator)
+  //console.log("found ",number," times")
   document.getElementById('result').textContent = number
 })
 
